1ba78e58fc94d95597c917fe14f577622db3cdbc
jsunghag\Hak <jshagold@gmail.com>
2017-10-14 22:46:38 +0900

    [10/14] add image, 레이아웃 공부(ListView, adapter) 구글링해서 예제를 따라해봤다.
    - 추가 -
    <ListViewItem.java>
    ListView에 표현할 함수구현.
    
    <listview_item.xml>
    ListView에 표현할 레이아웃 구성.
    
    <ListViewAdapter.java>
    BaseAdapter를 상속.
    
    - 수정 -
    <FirstViewActivity.java>
    FloatingActionButton, NavigationBar, DrawerLayout은 걸리적거려서 삭.제.
    list_view, ListViewAdapter생성.

--- Homework/app/src/main/java/com/example/hak/homework/FirstviewActivity.java
+++ Homework/app/src/main/java/com/example/hak/homework/FirstviewActivity.java
@@ -1,46 +1,53 @@
 package com.example.hak.homework;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class FirstviewActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.firstview);
+        setContentView(R.layout.content_firstview);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+//        // list view기본사용
+//        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU);
+//
+//        final ListView listView = (ListView) findViewById(R.id.right_layout);
+//        listView.setAdapter(adapter);
+//
+//
+        // 커스텀 list view 생성.
+        ListView custom_listview;
+        ListViewAdapter custom_adapter;
+            // Adapter 생성
+        custom_adapter = new ListViewAdapter();
+            // 리스트뷰 참조 및 Adapter닫기
+        custom_listview = (ListView) findViewById(R.id.left_layout);
+        custom_listview.setAdapter(custom_adapter);
+            // 첫번째 아이템 추가
+        custom_adapter.addItem(ContextCompat.getDrawable(this, R.drawable.background), "background", "Background Image");
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // ExpandableListView
+
     }
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -98,4 +105,10 @@ public class FirstviewActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // list view데이터
+    static final String[] LIST_MENU = {"LIST1","LIST2","LIST3"};
+
+
+
 }
