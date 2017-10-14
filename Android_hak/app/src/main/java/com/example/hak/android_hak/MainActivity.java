264ad6b0d51bfce78675ea756cec0f80db939a4e
jsunghag\Hak <jshagold@gmail.com>
2017-10-14 22:36:26 +0900

    [10/14] 기본설정수정

--- Android_hak/app/src/main/java/com/example/hak/android_hak/MainActivity.java
+++ Android_hak/app/src/main/java/com/example/hak/android_hak/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hak.android_hak;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstview);
+
+        ExpandableListView mainListView = (ExpandableListView)findViewById(R.id.main_list);
+
+        BaseExpandableAdapter adapter = new BaseExpandableAdapter(this);
+
+        ArrayList<String> child = new ArrayList<String>();
+        child.add(0, "first");
+        child.add(1, "second");
+        adapter.addTest("One", child, 0);
+
+        ArrayList<String> child2 = new ArrayList<String>();
+        child2.add(0, "dul");
+        child2.add(1, "set");
+        adapter.addTest("Two", child2, 1);
+
+        mainListView.setAdapter(adapter);
+
     }
+
 }
