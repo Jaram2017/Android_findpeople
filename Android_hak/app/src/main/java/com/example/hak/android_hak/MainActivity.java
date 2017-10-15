2c775af6d5187586083acf1ab0d4b64e03818f70
jsunghag\Hak <jshagold@gmail.com>
2017-10-16 01:21:33 +0900

    [10/16] - 추가 - <start_activity.xml> text만 보여줄 예시layout
    <StartActivity.java>
    레이아웃을 변경만 하게 해주는 activity.(SetContentView()사용)
    
    - 수정 -
    <firstview.xml>
    버튼을 활용하기위해 id부여
    
    <AndroidManifest.xml>
    Activity를 새로 만들어서 <activity>태그를 하나더 만듬(등록).
    
    <MainActivity.java>
    button을 눌렀을때 activity를 작동시키기위해 button에 setOnClickListener사용.
    activity를 전환시키기위해서 Intent로 StartActivity를 지정하고 startActivity()를 사용해서 activity전환

--- Android_hak/app/src/main/java/com/example/hak/android_hak/MainActivity.java
+++ Android_hak/app/src/main/java/com/example/hak/android_hak/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hak.android_hak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -30,10 +32,17 @@ public class MainActivity extends AppCompatActivity {
 
         mainListView.setAdapter(adapter);
 
-        Intent intent = new Intent(this, StartActivity.class);
 
-        startActivity(intent);
+        final Intent intent = new Intent(this, StartActivity.class);
 
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- Android_hak/app/src/main/java/com/example/hak/android_hak/StartActivity.java
+++ Android_hak/app/src/main/java/com/example/hak/android_hak/StartActivity.java
@@ -1,9 +1,18 @@
 package com.example.hak.android_hak;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Hak on 2017-10-16.
  */
 
-public class StartActivity {
+public class StartActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.start_activity);
+    }
 }
